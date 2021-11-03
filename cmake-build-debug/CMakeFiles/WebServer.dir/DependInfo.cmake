cec4237d7860b91c54eace71078ba2793a7fd37f
mqueen21 <76485564+mqueen21@users.noreply.github.com>
2021-11-03 20:44:10 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/WebServer.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lexa/CLionProjects/WebServer/test/HTTP/HttpRequest.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/HTTP/HttpRequest.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/HTTP/HttpResponse.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/HTTP/HttpResponse.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Parser.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Parser.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Server.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Server.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Servers/Server.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Servers/Server.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Servers/TestServer.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Servers/TestServer.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Sockets/ASocket.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Sockets/ASocket.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Sockets/ClientSocket.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Sockets/ClientSocket.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/Sockets/ServerSocket.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/Sockets/ServerSocket.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/WebServer.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/WebServer.cpp.o"
+  "/home/lexa/CLionProjects/WebServer/test/main.cpp" "/home/lexa/CLionProjects/WebServer/cmake-build-debug/CMakeFiles/WebServer.dir/test/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../test/HTTP"
+  "../test/Servers"
+  "../test/Sockets"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
